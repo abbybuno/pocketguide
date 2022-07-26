5268b7b9cc83e1498f9e994f4160aedd355db1bb
abbybuno <acbuno@donbosco.edu.ph>
2022-07-26 11:58:58 +0800

    Adding the SQL File

--- pocketguide.sql
+++ pocketguide.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2022 at 06:00 AM
+-- Generation Time: Jul 26, 2022 at 05:55 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,16 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_announcements`
+--
+
+CREATE TABLE `tbl_announcements` (
+  `a_id` int(100) NOT NULL,
+  `date_posted` varchar(100) NOT NULL,
+  `a_author` varchar(200) NOT NULL,
+  `a_subject` varchar(200) NOT NULL,
+  `a_content` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_accounts`
 --
 
 CREATE TABLE `user_accounts` (
   `uid` int(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
   `fullname` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `user_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_accounts`
+--
+
+INSERT INTO `user_accounts` (`uid`, `email`, `fullname`, `password`, `user_type`) VALUES
+(1, 'acbuno@donbosco.edu.ph', 'abi', '123', '1');
 
 --
 -- Indexes for dumped tables
@@ -53,7 +74,7 @@ ALTER TABLE `user_accounts`
 -- AUTO_INCREMENT for table `user_accounts`
 --
 ALTER TABLE `user_accounts`
-  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
